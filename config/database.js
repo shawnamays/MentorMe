f7e087fb2c49ccba3dc7ae1919afc4aa02eaa849
Shawna Mays <shawnadmays@gmail.com>
2022-12-29 23:21:21 -0500

    first commit MentorMe

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://geminimoonchild:1234@cluster0.o3h3upz.mongodb.net/MentorMe?retryWrites=true&w=majority', 
+    'dbName': 'MentorMe'
+};
